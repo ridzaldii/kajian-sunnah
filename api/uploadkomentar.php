80de284a1abbacf36c75b52d3ab4626b07598780
ridzaldii <rzaldy14@gmail.com>
2018-07-03 18:44:02 +0800

    Add API for comment

---
+++ api/uploadkomentar.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once 'Connection.php';
+
+$response = array("error" => FALSE);
+
+$conn = new Connection();
+$MySQLiconn = $conn->getConnection();
+
+if (isset($_POST['id_artikel']) && isset($_POST['nama']) && isset($_POST['komentar'])) {
+ 
+ $id_artikel = htmlspecialchars($_POST['id_artikel']);
+ $nama = htmlspecialchars($_POST['nama']);
+ $komentar = htmlspecialchars($_POST['komentar']);
+        
+ $sql = $MySQLiconn->query("INSERT INTO komentar VALUES ('','$id_artikel','$nama','$komentar')");
+
+     if($sql) {
+         $response["error"] = FALSE;
+         $response["message"] = "Pengiriman komentar berhasil.";
+
+         echo json_encode($response);
+      } else {
+        $response["error"] = TRUE;
+        $response["message"] = "Pengiriman komentar gagal.";
+
+        echo json_encode($response);
+      }    
+}
+?>
